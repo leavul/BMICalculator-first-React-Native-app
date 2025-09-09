5023c7588616d34f71b5e995e24a6bb3faff116e
Albaraa <albaraa.yadag@gmail.com>
2025-09-09 20:26:20 +0300

    style: change status bar to dark style

--- app/_layout.js
+++ app/_layout.js
@@ -1,18 +1,22 @@
 import { Stack } from "expo-router";
+import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{
-          title: 'BMI Calculator',
-          headerTitleStyle: {
-            fontFamily: 'Ubuntu Bold',
-          }
-        }}
-      >
-      </Stack.Screen>
-    </Stack >)
-    ;
+    <>
+      <StatusBar style="dark" />
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            title: 'BMI Calculator',
+            headerTitleStyle: {
+              fontFamily: 'Ubuntu Bold',
+            }
+          }}
+        >
+        </Stack.Screen>
+      </Stack >
+    </>
+  );
 }
